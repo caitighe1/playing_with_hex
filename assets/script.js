5049abbfdb87a3c9ea6d5264788a7c0e2de2d659
Caitlin Tighe <caitighe@gmail.com>
2021-07-08 20:43:57 -0400

    added event listener and for loop that will shuffle through hex codes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 //created an array with all of the possible combinations for hex code
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const button = document.getElementById('button')
+//created two variables to grab each element
+const button = document.getElementById('btn')
 const color = document.querySelector('color');
+
+//when the user clicks the button, function with work through a for loop grabbing six random letters and 
+//numbers from the array and then will be added to the #
+btn.addEventListener("click", function() {
+    let hexCode = "#";
+    for (let i= 0; i <6; i++) {
+       hexCode += hex[getRandomNumber()];
+       
+       document.body.style.backgroundColor = hexCode;
+       color.textContent = hexCode;
+    }
+} )
