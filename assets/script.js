f508f44719f7b329a1f924ec3e5fe796cc7a8a1e
Caitlin Tighe <caitighe@gmail.com>
2021-07-08 21:17:30 -0400

    added readme template

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 //created two variables to grab each element
 const button = document.getElementById('btn')
-const color = document.querySelector('color');
+const color = document.querySelector('.color');
 
 //when the user clicks the button, function with work through a for loop grabbing six random letters and 
 //numbers from the array and then will be added to the #
@@ -15,4 +15,10 @@ btn.addEventListener("click", function() {
        document.body.style.backgroundColor = hexCode;
        color.textContent = hexCode;
     }
-} )
+})
+
+//creating a function that will randomly choose any of the hex array items
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
